469b727ca4953471c142f2726ab9e2ca3656759b
Tion <tion.jamesdominic@gmail.com>
2026-04-15 19:44:25 +0800

    updated parallel search

--- GroupActivity-1/parallel_search.py
+++ GroupActivity-1/parallel_search.py
@@ -1,6 +1,5 @@
 from multiprocessing import Process, Queue
 
-
 def worker(sub_data, target, q, offset):
     for i in range(len(sub_data)):
         if target == sub_data[i]:
@@ -9,14 +8,27 @@ def worker(sub_data, target, q, offset):
     q.put(-1)
 
 
-
-
-def paralle_search(data, target):
+def parallel_search(data, target):
     processes = []
     q = Queue()
 
     chunk_size = len(data) // 4
 
     for i in range(4):
+        offset = chunk_size * i
+        sub_data = data[offset:offset+chunk_size]
+        p = Process(target=worker, args=(sub_data, target, q, offset))
+        processes.append(p)
+        p.start()
 
+    result = -1
+    for _ in processes:
+        res = q.get()
+        if res != -1:
+            result = res
+
+    for p in processes:
+        p.join()
+
+    return result
 
